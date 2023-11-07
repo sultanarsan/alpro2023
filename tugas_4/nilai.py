2ebb5f8e215a6a2758c490df00c9d2e8ce377709
sultanarsan <arsan.22315034@umkendari.ac.id>
2023-11-07 08:58:00 +0800

    tugas 4 selesai

---
+++ tugas_4/nilai.py
@@ -0,0 +1,16 @@
+
+if __name__ == "__main__":
+	nilai = int(input ('masukkan nilai: '))
+	
+	if nilai <= 30:
+		print('E')
+	elif nilai <= 55:
+		print('D')
+	elif nilai <= 70:
+		print('C')
+	elif nilai <= 85:
+		print('B')
+	else:
+		print('A')
+			
+	print('alhamdulillah selesai')			
